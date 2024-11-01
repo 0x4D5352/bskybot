ddd1f893f0bb3c09058d14f90a65d523b54a9ee2
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-10-31 21:53:45 -0500

    implement working get-started

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/0x4D5352/bskybot
 
 go 1.23.2
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,42 +1,50 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"io"
+	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
+	"time"
 )
 
-// Things I need to make this work:
-
-/*
-- an agent that is instantiated with the following at minimum:
-  - a service (https://bsky.social)
-  - a login method
-  - USE OAUTH
-  - a post method
-*/
-
 func main() {
+	fmt.Println("Grabbing login details...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	username := os.Getenv("BLUESKY_HANDLE")
 	appPassword := os.Getenv("BLUESKY_PASSWORD")
-	agent := BskyAgent{
-		client: &http.Client{},
-		host:   "https://bsky.social",
+	fmt.Println("Creating new session...")
+	agent := &BskyAgent{
+		Client:  &http.Client{},
+		Host:    "https://bsky.social",
+		Session: SessionToken{},
+		Posts:   []Posts{},
 	}
 	agent.login(username, appPassword)
+	fmt.Println("Connected!")
+	fmt.Println("Posting...")
+	agent.post(fmt.Sprintf("i'm posting this at %s!!!!!!!!!", time.Now().Format(time.RFC3339)))
+	fmt.Println("Shutting down...")
+	os.Exit(0)
 }
 
 type BskyAgent struct {
-	client  *http.Client
-	session *SessionToken
-	host    string
+	Client  *http.Client
+	Session SessionToken
+	Host    string
+	Posts   []Posts
 }
 
 type SessionToken struct {
-	Did    string `json:"did"`
-	DidDoc struct {
+	DID    string `json:"did"`
+	DIDDoc struct {
 		Context            []string `json:"@context"`
 		ID                 string   `json:"id"`
 		AlsoKnownAs        []string `json:"alsoKnownAs"`
@@ -56,28 +64,111 @@ type SessionToken struct {
 	Email           string `json:"email"`
 	EmailConfirmed  bool   `json:"emailConfirmed"`
 	EmailAuthFactor bool   `json:"emailAuthFactor"`
-	AccessJwt       string `json:"accessJwt"`
-	RefreshJwt      string `json:"refreshJwt"`
+	AccessJWT       string `json:"accessJwt"`
+	RefreshJWT      string `json:"refreshJwt"`
 	Active          bool   `json:"active"`
 }
 
+type Posts struct {
+	Post     Post
+	Response PostResponse
+}
 type Post struct {
-	Repo       string `json:"repo"`
-	Collection string `json:"collection"`
-	Record     struct {
-		Text      string `json:"text"`
-		CreatedAt string `json:"createdAt"`
-	} `json:"record"`
-	Response struct {
-		URI string `json:"uri"`
-		CID string `json:"cid"`
+	Repo       string     `json:"repo"`
+	Collection string     `json:"collection"`
+	Record     PostRecord `json:"record"`
+}
+
+type PostRecord struct {
+	Text      string `json:"text"`
+	CreatedAt string `json:"createdAt"`
+}
+
+type PostResponse struct {
+	URI string `json:"uri"`
+	CID string `json:"cid"`
+}
+
+func (b *BskyAgent) login(u, p string) {
+	login := struct {
+		Identifier string `json:"identifier"`
+		Password   string `json:"password"`
+	}{
+		Identifier: u,
+		Password:   p,
 	}
+	payload, err := json.Marshal(login)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body := bytes.NewReader(payload)
+	fmt.Println("trying to login....")
+	resp, err := b.Client.Post(b.Host+"/xrpc/com.atproto.server.createSession", "application/json", body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if resp.StatusCode > 299 {
+		for k, v := range resp.Header {
+			fmt.Printf("Key: %s, Value: %s\n", k, v)
+		}
+		log.Fatalf("Response failed with status code: %d and\nbody %s\n", resp.StatusCode, result)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(result, &b.Session)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Logged in as %s!\n", b.Session.Handle)
 }
 
-func (b BskyAgent) login(u, p string) {
-}
-
-func (b BskyAgent) post(c string) {
-	body := strings.NewReader("test post")
-	b.client.Post("test", "application/json", body)
+func (b *BskyAgent) post(c string) {
+	createdAt := time.Now().Format(time.RFC3339)
+	post := Post{
+		Repo:       b.Session.Handle,
+		Collection: "app.bsky.feed.post",
+		Record: PostRecord{
+			Text:      c,
+			CreatedAt: createdAt,
+		},
+	}
+	payload, err := json.Marshal(post)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body := bytes.NewReader(payload)
+	req, err := http.NewRequest("POST", b.Host+"/xrpc/com.atproto.repo.createRecord", body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", b.Session.AccessJWT))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := b.Client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if resp.StatusCode > 299 {
+		for k, v := range resp.Header {
+			fmt.Printf("Key: %s, Value: %s\n", k, v)
+		}
+		log.Fatalf("Response failed with status code: %d and\nbody %s\n", resp.StatusCode, result)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	var response PostResponse
+	err = json.Unmarshal(result, &response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b.Posts = append(b.Posts, Posts{
+		Post:     post,
+		Response: response,
+	})
+	fmt.Printf("Posted %s at %s!\n", c, createdAt)
 }
